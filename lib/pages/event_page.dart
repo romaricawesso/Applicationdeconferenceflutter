8aff69d526d4601b4af4f2218cf34ea9d58ade4c
romaricawesso <awessoromaric058@gmail.com>
2026-01-11 01:19:16 +0100

    pop dialogue

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,3 +1,4 @@
+import 'package:applicationdeconference/models/event_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -33,9 +34,50 @@ class _EventPageState extends State<EventPage> {
 
   @override
   Widget build(BuildContext context) {
+
+
+    Future<void> showEventDetailsDialog(Event eventData) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: true, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Conference ${eventData.speaker}'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Image.asset("assets/images/${eventData.avatar}.jpg",height: 120,),
+                  Text('Titre : ${eventData.subject}'),
+                  Text('Speaker: ${eventData.speaker}'),
+                  Text('Date de la conf: ${DateFormat.yMd().add_jm().format(eventData.timestamp.toDate())}'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              ElevatedButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.calendar_month),
+                label: Text("Ajouter calendrier")
+              ),
+              SizedBox(width: 50,),
+              TextButton(
+                child: const Text('Fermer'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Center(
       child: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('events').orderBy('date').snapshots(),
+        stream: FirebaseFirestore.instance
+            .collection('events')
+            .orderBy('date')
+            .snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
@@ -45,26 +87,33 @@ class _EventPageState extends State<EventPage> {
             return Text("Aucune conference");
           }
 
-          List<dynamic> events = [];
-          snapshot.data!.docs.forEach((element) {
-            events.add(element);
+          List<Event> events = [];
+          snapshot.data!.docs.forEach((data) {
+            events.add(Event.fromData(data));
           });
           return ListView.builder(
             itemCount: events.length,
             itemBuilder: (context, index) {
               final event = events[index];
-              final avatar = event['avatar'].toString().toLowerCase();
-              final speaker = event['speaker'];
-              final Timestamp timestamp = event['date'];
-              final String date = DateFormat.yMd().add_jm().format(timestamp.toDate());
-              final subject = event['subject'];
+              final avatar = event.avatar;
+              final speaker = event.speaker;
+              final Timestamp timestamp = event.timestamp;
+              final String date = DateFormat.yMd().add_jm().format(
+                timestamp.toDate(),
+              );
+              final subject = event.subject;
 
               return Card(
                 child: ListTile(
                   leading: Image.asset("assets/images/$avatar.jpg"),
                   title: Text('$speaker($date)'),
                   subtitle: Text('$subject'),
-                  trailing: Icon(Icons.more_vert),
+                  trailing: IconButton(
+                    icon: Icon(Icons.info),
+                    onPressed: () {
+                    showEventDetailsDialog(event);
+                    },
+                  ),
                 ),
               );
             },
